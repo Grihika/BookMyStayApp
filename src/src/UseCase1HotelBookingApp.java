6b319feb06e162db3dbbeac936e869a9adc43e7d
Grihika <grihikajeloka@gmail.com>
2026-04-06 14:14:32 +0530

    welcom

---
+++ src/src/UseCase1HotelBookingApp.java
@@ -0,0 +1,11 @@
+public class UseCase1HotelBookingApp {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+
+        System.out.println("Application Name: Book My Stay App");
+        System.out.println("Version: 1.0");
+        System.out.println("Thank you for using the system!");
+    }
+}git
